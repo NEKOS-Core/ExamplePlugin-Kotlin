678e3b997a3f566ddc0bacdd8d08179f0efe31ce
diademiemi <emilia@diademiemi.me>
2022-04-02 13:33:00 +0200

    Simplify example plugin even more

--- src/main/kotlin/pet/nekos/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/pet/nekos/exampleplugin/ExamplePlugin.kt
@@ -3,19 +3,11 @@ package pet.nekos.exampleplugin
 import pet.nekos.api.plugin.Plugin
 import pet.nekos.api.event.Listener
 import pet.nekos.api.event.EventHandler
-import pet.nekos.api.event.EventPriority
 import pet.nekos.api.event.message.MessageEvent
 
-import java.io.File
-
 class ExamplePlugin() : Plugin(), Listener {
- 
-    override fun onEnable() {
-        this.server?.pluginManager?.registerEvents(this, this)
-        isEnabled = true
-    }
 
-    @EventHandler(EventPriority.NORMAL)
+    @EventHandler
     fun onMessage(e: MessageEvent) {
         println("Very WIP! Testing message... Content passed along was: " + e.content)
     }   

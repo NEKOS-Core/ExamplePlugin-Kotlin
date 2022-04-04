c1b7aeb2bd84601c28f1ce2cf9f9346db318f1a6
diademiemi <emilia@diademiemi.me>
2022-04-04 23:57:08 +0200

    Better testing

--- src/main/kotlin/pet/nekos/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/pet/nekos/exampleplugin/ExamplePlugin.kt
@@ -5,11 +5,17 @@ import pet.nekos.api.event.Listener
 import pet.nekos.api.event.EventHandler
 import pet.nekos.api.event.message.MessageEvent
 
+import pet.nekos.api.message.ChatMessage
+
 class ExamplePlugin() : Plugin(), Listener {
 
     @EventHandler
     fun onMessage(e: MessageEvent) {
-        println("Very WIP! Testing message... Content passed along was: " + e.content)
+        println(e.msg)
+        println("Very WIP!" + "\nName: " + e.msg.user.name + "\nNickname: " + e.msg.user.nickname + "\nContent: " + e.msg.content)
+        if (e.msg is ChatMessage) {
+            (e.msg as ChatMessage).reply("Testing")
+        }
     }
 
 }
